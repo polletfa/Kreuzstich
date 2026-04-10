a84c9db11e4f7792d2b5b55bb2eb8a6f2478da08
Fabien Pollet <polletfa@posteo.de>
2026-04-10 20:43:18 +0200

    rename colortable to threadlist

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ if [ "$MODE" != "debug" -a "$MODE" != "release" ]; then
     echo "   $0 debug|release [<make arguments>]"
     echo
     echo "Run cmake for the specified build and call make with the specified arguments"
+    echo
+    echo "Useful targets:"
+    echo "  all          Build everything"
+    echo "  unittests    Build unit tests"
+    echo "  test         Run unit tests (no rebuild)"
+    echo "  coverage     Build, run test and create coverage report"
     exit 1
 fi
 cmake -B _build/$MODE -DCMAKE_BUILD_TYPE=$MODE
